67d3ed63baf36de53c7e019a9075346e04bba36b
Fa Dede <fanny.devilliers@gmail.com>
2021-03-31 18:48:19 +0200

    index(add)displays tags for each photographer OK

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ fetch("datas.json")
 
   .then((data) => {
     data.photographers.forEach((photographer) => {
+      let arrayTags = photographer.tags; // tableaux des tags des photographes
+      let tag = arrayTags.map((tag) => `<li class ='tags'> ${tag} </li>`); // méthode qui permet d'isoler les éléments du tableau + de les incrémenter entre deux balises <li>
+      let tags = `<ul class='tag-list'> ${tag.join("")} </ul>`;
+      // méthode.join qui incrémente mes <li> dans une <ul>
+
       let mainContainer = document.querySelector(".main-container");
       mainContainer.innerHTML += `
       <article id="first-profile" class="container-profile">
-      <a href="./photographer-page.html?id=${photographer.id}">
-        <figure>
-          <img
-            class="container-profile__picture"
-            src="./img/Portraits/${photographer.portrait}"
-            alt="${photographer.name}'s profile"
-          />
-        </figure>
+        <a href="./photographer-page.html?id=${photographer.id}">
+          <figure>
+            <img
+              class="container-profile__picture"
+              src="./img/Portraits/${photographer.portrait}"
+             alt="${photographer.name}'s profile"
+            />
+          </figure>
 
-        <h2 class="container-profile__name">${photographer.name}</h2>
-      </a>
+          <h2 class="container-profile__name">${photographer.name}</h2>
+        </a>
 
-      <div class="container-profile__information">
-        <h3 class="location">${photographer.city}, ${photographer.country}</h3>
-        <h4 class="quote">${photographer.tagline}</h4>
-        <h5 class="prices">${photographer.price} € / jour</h5>
-      </div>
-
-      <ul id='tag-list'>
-  
-      </ul>
-    </article>`;
-
-      photographer.tags.forEach((tag) => {
-        document.getElementById(
-          "tag-list"
-        ).innerHTML += `<li class="tags">${tag}</li> `;
-      });
+        <div class="container-profile__information">
+          <h3 class="location">${photographer.city}, ${photographer.country}</h3>
+          <h4 class="quote">${photographer.tagline}</h4>
+          <h5 class="prices">${photographer.price} € / jour</h5>
+        </div>
+        ${tags}
+      </article>`;
     });
   });
+
+// let tags = arrayTags.map((tag) => "<li>" + tag + "</li>");
+
+// let html = `<ul> ${tags.join("")}</ul>`;
+// console.log(html);
