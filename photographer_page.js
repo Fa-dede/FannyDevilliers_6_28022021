9fc9521bd3169d6087300422a25e2a7545aa3b27
Fa Dede <fanny.devilliers@gmail.com>
2021-04-07 15:15:51 +0200

    PP(js)filter with dates

--- photographer_page.js
+++ photographer_page.js
@@ -40,13 +40,33 @@ const init = async (idPhotographer) => {
 
       let totalNumberOfLikesArray = [];
       let heartArray = document.getElementsByClassName("hearts");
+      let mediaArray = [];
+      let dateArray = [];
+      let popularityArray = [];
 
       //Boucle2 : boucle les médias
       datas.media.forEach((media) => {
         // condition 2.1 qui filtre les médias si la valeur PhotographerId = à mon paramètre idPhotographer
-
         if (media.photographerId == idPhotographer) {
-          document.querySelector("#portfolio").innerHTML += `
+          //incrémente les médias dans un tableau Js
+          mediaArray.push(media);
+          dateArray.push(media.date);
+          popularityArray.push(media.likes);
+        }
+      });
+
+      // méthode qui permet de trier mes dates de la plus récente à la plus  ancienne
+      let dateRecentArray = dateArray.sort().reverse();
+      console.log(dateRecentArray);
+      console.log(popularityArray);
+
+      // boucle sur le tableau des dates
+      dateRecentArray.forEach((date) => {
+        // boucle interne au tableau des dates qui boucle chaque média
+        mediaArray.forEach((media) => {
+          //condition qui permet d'afficher mes médias en suivant l'ordre du tableau de date
+          if (media.date === date) {
+            document.querySelector("#portfolio").innerHTML += `
             <figure class="photo">
                 <img
                     class="photo-picture"
@@ -63,9 +83,14 @@ const init = async (idPhotographer) => {
                 </footer>
             </figure>`;
 
-          totalNumberOfLikesArray.push(media.likes);
-        } //Fin de ma condition 2.1 : if (media.photographerId == idPhotographer)
-      }); //Fin de Boucle 2 : datas.media.forEach()
+            totalNumberOfLikesArray.push(media.likes);
+          }
+        });
+      });
+
+      // Boucle 3 : boucle les média du tableau créé ci-dessus pour les afficher
+
+      //Fin de Boucle 3 : mediaArray.forEach()
 
       //Clic sur les photos pour faire apparaitre la lightbox
 
