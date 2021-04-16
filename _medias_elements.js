53074d393bbfbe6143665d120fc24ef56440f49f
Fa-dede <75364869+Fa-dede@users.noreply.github.com>
2021-04-16 09:15:35 +0200

    Add files via upload

---
+++ _medias_elements.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @param {array} medias
+ * @param {number} photographerID
+ */
+
+const addPhotos = (medias, photographerID) => {
+  medias.forEach((media) => {
+    if (media.photographerId == photographerID && media.image) {
+      document.querySelector("#portfolio").innerHTML += `
+                <figure class="photo">
+                    <img
+                        class="photo-picture"
+                        src="./img/${media.photographerId}/${media.image}"
+                        alt=""
+                        />
+    
+                    <footer class="photo-footer">
+                    <span class="photo-footer__title">${media.title}</span>
+                    <span class="photo-footer__likes"
+                        ><span class="heart-txt">${media.likes}</span> <i class="hearts far fa-heart"></i
+                    ></span>
+                    <span class="photo-footer__price">${media.price} €</span>
+                    </footer>
+                </figure>`;
+    }
+  });
+};
+
+const addVideos = (medias, photographerID) => {
+  medias.forEach((media) => {
+    if (media.photographerId == photographerID && media.video) {
+      document.querySelector("#portfolio").innerHTML += `
+      <figure class="photo">
+      <video alt preload loop autoplay class = 'photo-picture' src="./img/${media.photographerId}/${media.video}">Ici la description alternative</video>
+      <footer class="photo-footer">
+                    <span class="photo-footer__title">${media.title}</span>
+                    <span class="photo-footer__likes"
+                        ><span class="heart-txt">${media.likes}</span> <i class="hearts far fa-heart"></i
+                    ></span>
+                    <span class="photo-footer__price">${media.price} €</span>
+                    </footer>
+                    </figure>`;
+    }
+  });
+};
+
+export { addPhotos, addVideos };
